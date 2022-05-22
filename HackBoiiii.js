c8edb82986df03990ea2ec05da22f01a46530814
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-05-22 14:42:27 -0300

    its ui

--- HackBoiiii.js
+++ HackBoiiii.js
@@ -3,6 +3,7 @@ import { Localization } from "./api/Localization";
 import { BigNumber } from "./api/BigNumber";
 import { theory } from "./api/Theory";
 import { Utils } from "./api/Utils";
+import {ui} from "./api/ui/UI";
 
 var id = "my_custom_theory_id";
 var name = "Exponential Idle Hack Boiiiiiiiiiii!!!";
